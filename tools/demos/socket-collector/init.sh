1b90586ad959fbccd659fadc24de6e31ee8f7298
Will Daly <widaly@microsoft.com>
2022-09-20 15:43:42 -0700

    wip

--- tools/demos/socket-collector/init.sh
+++ tools/demos/socket-collector/init.sh
@@ -2,6 +2,7 @@
 
 kubectl delete ns demo || true
 kubectl create ns demo
+kubectl delete -n gadget socket-collector || true
 
 # Make sure the busybox image is downloaded
 kubectl run --rm -ti --restart=Never -n demo loader --image busybox -- sh -c 'echo OK'
