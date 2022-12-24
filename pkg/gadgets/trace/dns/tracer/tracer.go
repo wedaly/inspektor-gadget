f7768730da427bf8fee2a493d376730201c20eb4
Will Daly <will.e.daly@gmail.com>
2022-12-24 10:00:25 -0800

    comment

--- pkg/gadgets/trace/dns/tracer/tracer.go
+++ pkg/gadgets/trace/dns/tracer/tracer.go
@@ -56,6 +56,7 @@ func NewTracer() (*Tracer, error) {
 			return nil, err
 		}
 
+		// Derive latency from the request/response timestamps.
 		if bpfEvent.Qr == 0 {
 			latencyCalc.storeDnsRequestTimestamp(bpfEvent.SaddrV6, bpfEvent.Id, bpfEvent.Timestamp)
 		} else {
