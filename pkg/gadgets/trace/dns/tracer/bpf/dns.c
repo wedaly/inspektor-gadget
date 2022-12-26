1b5efdc19c309bbb51a276ad96d6b9d74aeade3b
Will Daly <will.e.daly@gmail.com>
2022-12-26 09:13:42 -0800

    wip

--- pkg/gadgets/trace/dns/tracer/bpf/dns.c
+++ pkg/gadgets/trace/dns/tracer/bpf/dns.c
@@ -130,8 +130,8 @@ static struct event_t build_event(struct __sk_buff *skb, union dnsflags flags, _
 		int ans_offset = DNS_OFF + sizeof(struct dnshdr) + name_len + 5;
 		if ((load_byte(skb, ans_offset) & 0xF0) == 0xC0) {
 			__u16 rrtype = load_half(skb, ans_offset + 2);
-			__u16 rrclass = load_half(skb, ans_offset + name_len + 4);
-			__u16 rdlength = load_half(skb, ans_offset + name_len + 8);
+			__u16 rrclass = load_half(skb, ans_offset + 4);
+			__u16 rdlength = load_half(skb, ans_offset + 8);
 
 			bpf_printk("DEBUG rrtype = %d", rrtype);
 			bpf_printk("DEBUG rrclass = %d", rrclass);
